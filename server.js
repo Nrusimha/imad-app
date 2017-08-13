780a47b2d41a14612add3d879c7c512e39cfe448
Nrusimha <gn.varma@gmail.com>
2017-08-13 15:18:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui', function (req, res) {
+  //res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  console.log('index1');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
